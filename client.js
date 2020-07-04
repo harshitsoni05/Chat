73db4b6119157f7057becb49862532741f9c7190
unknown <soni.harsh779@gmail.com>
2020-07-04 23:41:35 +0530

    make it better

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-var conn = new WebSocket('ws://52.73.214.99:9090'); 
+var conn = new WebSocket('ws://172.18.247.206:43560'); 
 
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
