bba14542720ebe7c306e703f3fb4f3c5b3e9909e
unknown <soni.harsh779@gmail.com>
2020-07-04 23:20:04 +0530

    make it better

--- clie.js
+++ clie.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-var conn = new WebSocket('wss://0.0.0.0:9090'); 
+var conn = new WebSocket('wss://0.0.0.0:process.env.PORT'); 
 
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
--- server.js
+++ server.js
@@ -5,7 +5,7 @@
 var WebSocketServer = require('ws').Server;
  
 //creating a websocket server at port 9090 
-var wss = new WebSocketServer({port: 9090}); 
+var wss = new WebSocketServer({port: process.env.PORT}); 
 
 //all connected to the server users 
 var users = {};
