7489b1296d4e1d0b0ac84c7759bc74c984a39de4
unknown <soni.harsh779@gmail.com>
2020-07-04 21:53:59 +0530

    make it better

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-var conn = new WebSocket('wss://0.0.0.0:9090'); 
+var conn = new WebSocket('wss://textchat123.herokuapp.com:9090'); 
 
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
