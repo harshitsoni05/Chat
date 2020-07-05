d59ba172a87e596fb0db5120bee77868424bea43
unknown <soni.harsh779@gmail.com>
2020-07-05 16:07:51 +0530

    make it better

--- clc.js
+++ clc.js
@@ -3,8 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-var conn = new WebSocket('ws://172.18.247.206:43560'); 
-
+var conn = new WebSocket('ws://localhost:9090'); 
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
 };
