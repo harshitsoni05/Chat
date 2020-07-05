f777bb15d96d0b1827eb5a291d7990ff15a846c6
unknown <soni.harsh779@gmail.com>
2020-07-05 17:03:10 +0530

    make it better

--- no.js
+++ no.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser; 
 
 //connecting to our signaling server 
-const conn = new WebSocket('wss://textchat123.herokuapp.com:8001');
+const conn = new WebSocket('wss://textchat123.herokuapp.com:42000');
 conn.onopen = function () { 
    console.log("Connected to the signaling server");
 };
