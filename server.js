ddaa3ded58fe06dd5da85a859e6e99b11f550d51
unknown <soni.harsh779@gmail.com>
2020-07-08 19:32:12 +0530

    make it better

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const { Server } = require('ws');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
+const client = '/client.js';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .use((req, res) => res.sendFile(client, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 const wss = new Server({ server });
