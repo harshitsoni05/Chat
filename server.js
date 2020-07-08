81e97e58b97bc093367df7fc24e3e2b445cdb44d
unknown <soni.harsh779@gmail.com>
2020-07-08 19:16:59 +0530

    make it better

--- server.js
+++ server.js
@@ -12,13 +12,3 @@ const server = express()
 
 const wss = new Server({ server });
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
-
-setInterval(() => {
-  wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
-  });
-}, 1000);
