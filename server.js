47343e35e3e9c7b58f8352c88538ab70983c57cb
unknown <soni.harsh779@gmail.com>
2020-07-08 17:42:46 +0530

    make it better

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = 'index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
