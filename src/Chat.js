298d0fa3c5ce828cc3e4d2bfc24878fb05bb3a0a
unknown <soni.harsh779@gmail.com>
2020-07-27 16:44:40 +0530

    make it better

--- src/Chat.js
+++ src/Chat.js
@@ -246,34 +246,6 @@ class Chat {
 }
 module.exports = Chat;
 
-  const messages = document.getElementById('msgContainer');
-
-function appendMessage() {
-	const message = document.getElementsByClassName('has-text-right is-size-6 right-aligned')[0];
-  const newMessage = message.cloneNode(true);
-  messages.appendChild(newMessage);
-}
-
-function getMessages() {
-	// Prior to getting your messages.
-  shouldScroll = messages.scrollTop + messages.clientHeight === messages.scrollHeight;
-  /*
-   * Get your messages, we'll just simulate it by appending a new one syncronously.
-   */
-  appendMessage();
-  // After getting your messages.
-  if (!shouldScroll) {
-    scrollToBottom();
-  }
-}
-
-function scrollToBottom() {
-  messages.scrollTop = messages.scrollHeight;
-}
-
-scrollToBottom();
-
-setInterval(getMessages, 100);
   
 
 
