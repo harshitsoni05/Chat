3a39cd3cb504b98e438869da1273b1c23cbf3cf0
unknown <soni.harsh779@gmail.com>
2020-08-09 18:01:41 +0530

    make it better

--- src/Chat.js
+++ src/Chat.js
@@ -126,7 +126,7 @@ class Chat {
       displayNotification(msg, code);
       clearTimer();
       if (msg == "Your Partner left." || msg == "Your time has ended." || msg == "You are disconnected") {
-        keyboard.style.display = "block";
+        keyboard.style.display = "none";
       }
     });
 
