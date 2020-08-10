56e75d5a9c4883cb8b3d7f37304243a7b94868de
unknown <soni.harsh779@gmail.com>
2020-08-10 19:45:55 +0530

    make it better

--- src/Chat.js
+++ src/Chat.js
@@ -100,7 +100,7 @@ class Chat {
       mainPage.style.display = "block";
       navbar.style.display = "flex";
       landingPage.style.display = "none";
-	  this._removeChild(document.getElementById("msgContainer"));
+	  that._removeChild(document.getElementById("msgContainer"));
 	  document.getElementById("msgContainer")="";
       if (keyboard.style.display == "none") {
         keyboard.style.display = "flex";
