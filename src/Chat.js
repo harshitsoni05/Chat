18e2b8d6ec19269c04f5a57ce64fe6347470a5a6
unknown <soni.harsh779@gmail.com>
2020-08-10 20:01:27 +0530

    make it better

--- src/Chat.js
+++ src/Chat.js
@@ -87,16 +87,14 @@ class Chat {
       if (info.style.display == "block") {
         info.style.display = "none";
       }
-      overlayBtn.classList.add("is-loading");
+      //overlayBtn.classList.add("is-loading");
       modal.classList.add("is-active");
     });
 
     this.socket.on("gotAPair", (user, otherUser) => {
       firstTime = false;
       notification.style.display = "none";
-      [modal, overlayBtn].forEach((e) => {
-        e.classList.remove("is-active");
-      });
+      
       mainPage.style.display = "block";
       navbar.style.display = "flex";
       landingPage.style.display = "none";
